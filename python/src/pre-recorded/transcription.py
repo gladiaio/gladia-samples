d795c5c20bc83eb06dc289dd0ac44213c82a00f6
Sami <sboudouk@student.42.fr>
2023-06-22 14:10:27 +0200

    ✨ feat: python pre-recorded & streaming examples

---
+++ python/src/pre-recorded/transcription.py
@@ -0,0 +1,31 @@
+import requests
+import os
+
+headers = {
+   'x-gladia-key': '', # Replace with your Gladia Token
+   'accept': 'application/json', # Accept json as a response, but we are sending a Multipart FormData
+}
+
+file_path = '../../../data/anna-and-sasha-16000.wav' # Change with your file path
+
+if os.path.exists(file_path): # This is here to check if the file exists
+  print("- File exists")
+else:
+  print("- File does not exist")
+
+file_name, file_extension = os.path.splitext(file_path) # Get your audio file name + extension
+
+with open(file_path, 'rb') as f:  # Open the file
+  files = {
+    # Sending a local audio file
+    'audio': (file_name, f, 'audio/'+file_extension[1:]), # Send it. Here it represents: (filename: string, file: BufferReader, fileMimeType: string)
+    # You can also send an URL for your audio file. Make sure it's the direct link and publicly accessible.
+    # 'audio_url': (None, 'http://files.gladia.io/example/audio-transcription/split_infinity.wav'),
+    # Then you can pass any parameters you wants. Please see: https://docs.gladia.io/reference/pre-recorded
+    'toggle_diarization': (None, True),
+  }
+  print('- Sending request to Gladia API...');
+  response = requests.post('https://api.gladia.io/audio/text/audio-transcription/', headers=headers, files=files)
+  print(response.text) # Get the results
+  print(response.status_code)
+  print('- End of work');
---
+++ python/src/streaming/live.py
@@ -0,0 +1,78 @@
+import asyncio
+import websockets
+import json
+import base64
+
+ERROR_KEY = 'error'
+TYPE_KEY = 'type'
+TRANSCRIPTION_KEY = 'transcription'
+LANGUAGE_KEY = 'language'
+
+# retrieve gladia key
+gladiaKey = ''  # replace with your gladia key
+if not gladiaKey:
+    print('You must provide a gladia key. Go to app.gladia.io')
+    exit(1)
+else:
+    print('using the gladia key : ' + gladiaKey)
+
+# connect to api websocket
+gladiaUrl = "wss://api.gladia.io/audio/text/audio-transcription"
+
+async def send_audio(socket):
+
+    # Configure stream with a configuration message
+    configuration = {
+        "x_gladia_key": gladiaKey,
+        # "model_type":"accurate" <- Less faster but more accurate model_type, useful if you need precise addresses for example.
+    }
+    await socket.send(json.dumps(configuration))
+
+    # Once the initial message is sent, send audio data
+    file = '../../../data/anna-and-sasha-16000.wav'
+    with open(file, 'rb') as f:
+        fileSync = f.read()
+    base64Frames = base64.b64encode(fileSync).decode('utf-8')
+    partSize = 20000  # The size of each part
+    numberOfParts = -(-len(base64Frames) // partSize)  # equivalent of math.ceil
+
+    # Split the audio data into parts and send them sequentially
+    for i in range(numberOfParts):
+        start = i * partSize
+        end = min((i + 1) * partSize, len(base64Frames))
+        part = base64Frames[start:end]
+
+        # Delay between sending parts (500 mseconds in this case)
+        await asyncio.sleep(0.5)
+        message = {
+            'frames': part
+        }
+        await socket.send(json.dumps(message))
+
+    await asyncio.sleep(2)
+    print("final closing")
+
+
+# get ready to receive transcriptions
+async def receive_transcription(socket):
+    while True:
+        response = await socket.recv()
+        utterance = json.loads(response)
+        if utterance:
+            if ERROR_KEY in utterance:
+                print(f"{utterance[ERROR_KEY]}")
+                break
+            else:
+                print(f"{utterance[TYPE_KEY]}: ({utterance[LANGUAGE_KEY]}) {utterance[TRANSCRIPTION_KEY]}")
+        else:
+            print('empty,waiting for next utterance...')
+
+
+# run both tasks concurrently
+async def main():
+    async with websockets.connect(gladiaUrl) as socket:
+        send_task = asyncio.create_task(send_audio(socket))
+        receive_task = asyncio.create_task(receive_transcription(socket))
+        await asyncio.gather(send_task, receive_task)
+
+asyncio.run(main())
