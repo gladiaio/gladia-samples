aa3457f2a79a8dd9b1857405aa50510fe80fe6a6
amorin-gladia <amorin@gladia.io>
2024-01-24 11:43:08 +0100

    feat: update python pre-recorded example

--- python/src/pre-recorded/transcription.py
+++ python/src/pre-recorded/transcription.py
@@ -1,37 +1,78 @@
 import requests
 import os
+from time import sleep
 
-headers = {
-   'x-gladia-key': '', # Replace with your Gladia Token
-   'accept': 'application/json', # Accept json as a response, but we are sending a Multipart FormData
-}
+
+def make_request(url, headers, method="GET", data=None, files=None):
+    if method == "POST":
+        response = requests.post(url, headers=headers, json=data, files=files)
+    else:
+        response = requests.get(url, headers=headers)
+    return response.json()
+
 
 print(os.getcwd())
-file_path = '../../../data/anna-and-sasha-16000.wav' # Change with your file path
+file_path = "../../../data/anna-and-sasha-16000.wav"  # Change with your file path
 
-if os.path.exists(file_path): # This is here to check if the file exists
-  print("- File exists")
+if os.path.exists(file_path):  # This is here to check if the file exists
+    print("- File exists")
 else:
-  print("- File does not exist")
+    print("- File does not exist")
+    exit(0)
 
-file_name, file_extension = os.path.splitext(file_path) # Get your audio file name + extension
 
-with open(file_path, 'rb') as f:  # Open the file
-  files = {
-    # Sending a local audio file
-    'audio': (file_name, f, 'audio/'+file_extension[1:]), # Send it. Here it represents: (filename: string, file: BufferReader, fileMimeType: string)
-    # You can also send an URL for your audio file. Make sure it's the direct link and publicly accessible.
-    # 'audio_url': (None, 'http://files.gladia.io/example/audio-transcription/split_infinity.wav'),
-    # Then you can pass any parameters you wants. Please see: https://docs.gladia.io/reference/pre-recorded
-    'toggle_diarization': (None, True),
-  }
-  print('- Sending request to Gladia API...');
-  response = requests.post('https://api.gladia.io/audio/text/audio-transcription/', headers=headers, files=files)
-  if response.status_code == 200:
-    print('- Request successful');
-    result = response.json()
-    print(result)
-  else:
-    print('- Request failed');
-    print(response.json())
-  print('- End of work');
+file_name, file_extension = os.path.splitext(
+    file_path
+)  # Get your audio file name + extension
+
+with open(file_path, "rb") as f:  # Open the file
+    file_content = f.read()  # Read the content of the file
+
+headers = {
+    "x-gladia-key": os.getenv("GLADIA_API_KEY", ""),  # Replace with your Gladia Token
+    "accept": "application/json",
+}
+
+files = [("audio", (file_path, file_content, "audio/" + file_extension[1:]))]
+
+print("- Uploading file to Gladia...")
+upload_response = make_request(
+    "https://api.gladia.io/v2/upload/", headers, "POST", files=files
+)
+print("Upload response with File ID:", upload_response)
+audio_url = upload_response.get("audio_url")
+
+data = {
+    "audio_url": audio_url,
+    "diarization": True,
+}
+# You can also send an URL directly without uploading it. Make sure it's the direct link and publicly accessible.
+# For any parameters, please see: https://docs.gladia.io/reference/pre-recorded
+
+headers["Content-Type"] = "application/json"
+
+print("- Sending request to Gladia API...")
+post_response = make_request(
+    "https://api.gladia.io/v2/transcription/", headers, "POST", data=data
+)
+
+print("Post response with Transcription ID:", post_response)
+result_url = post_response.get("result_url")
+
+if result_url:
+    while True:
+        print("Polling for results...")
+        poll_response = make_request(result_url, headers)
+
+        if poll_response.get("status") == "done":
+            print("- Transcription done: \n")
+            print(poll_response.get("result"))
+            break
+        elif poll_response.get("status") == "error":
+            print("- Transcription failed")
+            print(poll_response)
+        else:
+            print("Transcription status:", poll_response.get("status"))
+        sleep(1)
+
+print("- End of work")
