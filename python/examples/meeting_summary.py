6ce1042381395a7626fd25b8865cb8957e6ccbd5
egenthon-cmd <egenthon@gladia.io>
2026-04-07 17:00:26 +0200

    fix: right path python from root python folder

--- python/examples/meeting_summary.py
+++ python/examples/meeting_summary.py
@@ -5,7 +5,8 @@ from gladiaio_sdk import GladiaClient
 gladia_client = GladiaClient(api_key="GLADIA_API_KEY").prerecorded()
 
 transcription = gladia_client.transcribe(
-    audio_url="../../data/call-center-example.mp4",
+    audio_url="../data/meeting-example.mp4",
+    # check all the summarization options at https://docs.gladia.io/chapters/audio-intelligence/summarization
     options={"summarization": True, "summarization_config": {"type": "bullet_points"}},
 )
 
